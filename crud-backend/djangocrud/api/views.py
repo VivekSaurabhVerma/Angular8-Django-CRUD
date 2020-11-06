ef53ce93603bab80b89731768edef178f73113d4
Vivek Verma <vivek.vsv.iitb@gmail.com>
2020-11-06 16:38:40 +0530

    Adding files

---
+++ crud-backend/djangocrud/api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from djangocrud.api.serializers import MovieSerializer, MovieMiniSerializer
+from .models import Movie
+from rest_framework.response import Response
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def list(self, request, *args, **kwargs):
+        movies = Movie.objects.all()
+        serializer = MovieMiniSerializer(movies, many=True)
+        return Response(serializer.data)
